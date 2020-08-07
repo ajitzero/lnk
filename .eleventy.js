7926dbd47f68186d2d6d0e53721986fe75ef7925
Ajit Panigrahi <ajitzero@gmail.com>
2020-08-07 17:57:14 +0530

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,3 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("logo.png");
-  eleventyConfig.addPassthroughCopy("favicon.ico");
-  eleventyConfig.addPassthroughCopy("safari-pinned-tab.svg");
   eleventyConfig.addPassthroughCopy("robots.txt");
 };
